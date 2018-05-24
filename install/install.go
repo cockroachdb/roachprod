a45be81ced0602a5374487acbcc311b19f753d51
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2018-05-24 17:58:11 -0400

    Add option to install haproxy

--- install/install.go
+++ install/install.go
@@ -10,6 +10,56 @@ import (
 // Memoizes cluster info for install operations
 var Clusters = map[string]*SyncedCluster{}
 
+var installCmds = map[string]string{
+	"cassandra": `
+echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | \
+	sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list;
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -;
+sudo apt-get update;
+sudo apt-get install -y cassandra;
+sudo service cassandra stop;
+`,
+
+	"gcc": `
+sudo apt-get update;
+sudo apt-get install -y gcc;
+`,
+
+	// graphviz and rlwrap are useful for pprof
+	"go": `
+sudo apt-get update
+sudo apt-get install -y graphviz rlwrap
+
+curl https://dl.google.com/go/go1.9.3.linux-amd64.tar.gz | sudo tar -C /usr/local -xz
+echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/go.sh > /dev/null
+sudo chmod +x /etc/profile.d/go.sh
+`,
+
+	"haproxy": `
+sudo apt-get update;
+sudo apt-get install -y haproxy;
+`,
+
+	"ntp": `
+sudo apt-get update;
+sudo apt-get install -y \
+  ntp \
+  ntpdate;
+`,
+
+	"tools": `
+sudo apt-get update;
+sudo apt-get install -y \
+  fio \
+  iftop \
+  iotop \
+  sysstat \
+  linux-tools-common \
+  linux-tools-4.10.0-35-generic \
+  linux-cloud-tools-4.10.0-35-generic;
+`,
+}
+
 func Install(c *SyncedCluster, args []string) error {
 	do := func(title, cmd string) error {
 		var buf bytes.Buffer
@@ -21,79 +71,13 @@ func Install(c *SyncedCluster, args []string) error {
 	}
 
 	for _, arg := range args {
-		switch arg {
-		case "cassandra":
-			cmd := `
-echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | \
-  sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list;
-curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -;
-sudo apt-get update;
-sudo apt-get install -y cassandra;
-sudo service cassandra stop;
-`
-			if err := do("cassandra", cmd); err != nil {
-				return err
-			}
-
-		case "gcc":
-			cmd := `
-sudo apt-get update;
-sudo apt-get install -y gcc;
-`
-			if err := do("gcc", cmd); err != nil {
-				return err
-			}
-
-		case "go":
-			// graphviz and rlwrap are useful for pprof
-			cmd := `
-sudo apt-get update
-sudo apt-get install -y graphviz rlwrap
-
-curl https://dl.google.com/go/go1.9.3.linux-amd64.tar.gz | sudo tar -C /usr/local -xz
-echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/go.sh > /dev/null
-sudo chmod +x /etc/profile.d/go.sh
-`
-
-			if err := do("go", cmd); err != nil {
-				return err
-			}
-
-		case "mongodb":
-			return fmt.Errorf("TODO(peter): unimplemented: mongodb")
-
-		case "ntp":
-			cmd := `
-sudo apt-get update;
-sudo apt-get install -y \
-  ntp \
-  ntpdate;
-`
-			if err := do("ntp", cmd); err != nil {
-				return err
-			}
-
-		case "postgres":
-			return fmt.Errorf("TODO(peter): unimplemented: postgres")
-
-		case "tools":
-			cmd := `
-sudo apt-get update;
-sudo apt-get install -y \
-  fio \
-  iftop \
-  iotop \
-  sysstat \
-  linux-tools-common \
-  linux-tools-4.10.0-35-generic \
-  linux-cloud-tools-4.10.0-35-generic;
-`
-			if err := do("tools", cmd); err != nil {
-				return err
-			}
-
-		default:
+		cmd, ok := installCmds[arg]
+		if !ok {
 			return fmt.Errorf("unknown tool %q", arg)
+		}
+
+		if err := do(arg, cmd); err != nil {
+			return err
 		}
 	}
 	return nil
