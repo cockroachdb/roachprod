0984301df9abae338083d0637d549b6bbf9bb3ba
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2018-10-23 08:42:01 +0200

    Disable host key checking in non-interactive sessions
    IP reuse is frequent in our GCE test clusters.

--- install/session.go
+++ install/session.go
@@ -32,6 +32,7 @@ type remoteSession struct {
 func newRemoteSession(user, host string) (*remoteSession, error) {
 	args := []string{
 		user + "@" + host,
+		"-o", "UserKnownHostsFile=/dev/null",
 		"-o", "StrictHostKeyChecking=no",
 	}
 	args = append(args, sshAuthArgs()...)
