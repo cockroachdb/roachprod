17113651db295713ba4e4d492fe9d5dcb8ca0d68
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2018-11-13 14:41:08 +0100

    Bump GCE xenial image
    The other one is already deprecated. Sigh.

--- vm/gce/gcloud.go
+++ vm/gce/gcloud.go
@@ -226,7 +226,7 @@ func (p *Provider) Create(names []string, opts vm.CreateOpts) error {
 		"--subnet", "default",
 		"--maintenance-policy", "MIGRATE",
 		"--scopes", "default,storage-rw",
-		"--image", "ubuntu-1604-xenial-v20181023",
+		"--image", "ubuntu-1604-xenial-v20181030",
 		"--image-project", "ubuntu-os-cloud",
 		"--boot-disk-size", "10",
 		"--boot-disk-type", "pd-ssd",
